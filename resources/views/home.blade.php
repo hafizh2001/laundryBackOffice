37c16e41e94b6752769960a8bc4979d97e3b5728
hafizh2001 <novacasan219@gmail.com>
2023-06-11 14:24:14 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends ('layouts.master')
+@section('content')
+<section class="content-header">
+    <div>
+        <h1>
+            heloo
+        </h1>
+    </div>
+</section>
+@endsection
---
+++ resources/views/layouts/navbar1.blade.php
@@ -0,0 +1,63 @@
+<!-- Navbar Dark -->
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-navabr1 z-index-3 py-3">
+    <div class="container">
+        <a class="navbar-brand text-white" href="https://demos.creative-tim.com/material-kit/presentation" rel="tooltip"
+            title="Designed and Coded by Creative Tim" data-placement="bottom" target="_blank">
+            Material Kit 2
+        </a>
+        <a href="https://www.creative-tim.com/product/material-design-system-pro#pricingCard"
+            class="btn btn-sm  bg-gradient-primary  btn-round mb-0 ms-auto d-lg-none d-block">Buy Now</a>
+        <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon mt-2">
+                <span class="navbar-toggler-bar bar1"></span>
+                <span class="navbar-toggler-bar bar2"></span>
+                <span class="navbar-toggler-bar bar3"></span>
+            </span>
+        </button>
+        <div class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0" id="navigation">
+            <ul class="navbar-nav navbar-nav-hover mx-auto">
+                <li class="nav-item mx-2">
+                    <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center"
+                        role="button">
+                        Pages
+                        <img src="../../assets/img/down-arrow-white.svg" alt="down-arrow" class="arrow ms-1">
+                    </a>
+                </li>
+
+                <li class="nav-item mx-2">
+                    <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center"
+                        role="button">
+                        Account
+                        <img src="../../assets/img/down-arrow-white.svg" alt="down-arrow" class="arrow ms-1">
+                    </a>
+                </li>
+                <li class="nav-item mx-2">
+                    <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center"
+                        role="button">
+                        Blocks
+                        <img src="../../assets/img/down-arrow-white.svg" alt="down-arrow" class="arrow ms-1">
+                    </a>
+                </li>
+
+                <li class="nav-item dropdown dropdown-hover mx-2">
+                    <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center"
+                        role="button">
+                        Docs
+                        <img src="../../assets/img/down-arrow-white.svg" alt="down-arrow" class="arrow ms-1">
+                    </a>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav d-lg-block d-none">
+                <li class="nav-item">
+                    <a href="https://www.creative-tim.com/product/material-design-system-pro#pricingCard"
+                        class="btn btn-sm  bg-gradient-primary  mb-0 me-1" role="button">Buy Now</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- End Navbar -->
